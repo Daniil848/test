f13bfc4ec7d076240b66b9ad5a54ba182846b65f
Daniil848 <ladygindaniil848@gmail.com>
2024-02-14 10:50:53 +0300

    header layout WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header/>
+      <Footer/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { AppBar, Toolbar, IconButton, } from "@mui/material";
+
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton>
+
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
