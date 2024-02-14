ba62a2d417d173a1709c15095bd5d0959f53c5ca
Daniil848 <ladygindaniil848@gmail.com>
2024-02-14 15:48:33 +0300

    sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,45 @@
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
+  const [state , setState] = useState(false);
+
   return (
-    <AppBar>
-      <Toolbar>
-        <IconButton color="inherit">
-          <MenuIcon/>
-        </IconButton>
-        <Typography color="Inherit" variant="h6">Menu</Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar>
+        <Toolbar>
+          <IconButton 						
+            size="large"
+						edge="start"
+						color={"inherit"}
+						aria-label="menu"
+						sx={{ mr: 2 }}
+						onClick={() => setState(true)
+          }>
+            <MenuIcon/>
+          </IconButton>
+          <Typography color="Inherit" variant="h6">Menu</Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+				anchor={'left'}
+				open={state}
+				onClose={() => setState(false)}
+				PaperProps={{
+					sx: {
+						bgcolor: 'grey'
+					}
+				}}>
+          <List>
+            <ListItem>
+              <Link to="/student">student</Link>
+            </ListItem>
+          </List>
+      </Drawer>
+    </>
   );
 };
 
