2383737e095287659367e9f7f94ab111f69db187
Daniil848 <ladygindaniil848@gmail.com>
2024-02-14 12:31:47 +0300

    student page WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainPage from './components/MainPage';
+import StudentPage from './components/StudentPage';
+import { Routes, Route } from 'react-router';
 import { Container } from '@mui/material';
 import './App.css';
 
@@ -9,7 +11,10 @@ function App() {
     <div className="App">
       <Header/>
       <Container maxWidth="sm" sx={{marginTop: "64px"}}>
-        <MainPage/>
+        <Routes>
+          <Route index element={<MainPage/>}/>
+          <Route path="/student" element={<StudentPage/>}/>
+        </Routes>
       </Container>
       <Footer/>
     </div>
