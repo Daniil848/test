16cbdd2206cc3178810369aae5455bf4e37e8542
Daniil848 <ladygindaniil848@gmail.com>
2024-02-14 14:19:53 +0300

    student page layout

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,30 +1,49 @@
-import { Box, List, ListItem, Typography, IconButton} from "@mui/material";
+import { Box, Stack, List, ListItem, Typography, IconButton, TextField, Button} from "@mui/material";
+import { Link } from "react-router-dom";
 import EditIcon from '@mui/icons-material/Edit';
 
 const MainPage = () => {
+
+  const styles = {
+    form : {
+      display : "flex",
+      flexDirection : "column",
+      width : "450px",
+      margin : "auto",
+    },
+  };
+
   return (
     <>
       <List>
-        <ListItem sx={{
-          display : "flex",
-          justifyContent : "space-between",
-          height : "50px",
-          borderBottom : 1,
-          borderColor : "grey.400"
-        }}>
-          <Typography>Bdfyjd Bdfy Bdfyjdbx</Typography>
-          <Box sx={{
+        <Link to={"/student"}>
+          <ListItem sx={{
             display : "flex",
-            alignItems : "center",
-            justifyContent : "space-between"
+            justifyContent : "space-between",
+            height : "50px",
+            borderBottom : 1,
+            borderColor : "grey.400"
           }}>
-            <Typography>Средний балл: 4,3</Typography>
-            <IconButton color="primary">
-              <EditIcon/>
-            </IconButton>
-          </Box>
-        </ListItem>
+            <Typography>Bdfyjd Bdfy Bdfyjdbx</Typography>
+            <Box sx={{
+              display : "flex",
+              alignItems : "center",
+              justifyContent : "space-between"
+            }}>
+              <Typography>Средний балл: 4,3</Typography>
+              <IconButton color="primary">
+                <EditIcon/>
+              </IconButton>
+            </Box>
+          </ListItem>
+        </Link>
       </List>
+      <Stack sx={styles.form} spacing={2}>
+        <TextField></TextField>
+        <TextField></TextField>
+        <TextField></TextField>
+        <Button variant="contained" size="large">OK</Button>
+      </Stack>
     </>
   );
 };
--- src/components/StudentPage.tsx
+++ src/components/StudentPage.tsx
@@ -1,6 +1,118 @@
+import { List, ListItem, Stack, Typography } from "@mui/material";
+
 const StudentPage = () => {
+
+  const styles = {
+    studentName : {
+      fontSize : "36px",
+      color : "primary"
+    },
+    list : {
+      
+    },
+    listItem : {
+      display : "flex",
+      justifyContent : "space-between",
+    },
+    courses : {
+      fontSize : "24px",
+      borderBottom : 2,
+      borderColor : "primary",
+      width : "fit-content",
+      margin : "auto",
+    },
+    courseTitle : {
+      fontSize : "20px"
+    },
+    grade : {
+      fontSize : "20px",
+      borderBottom : 1,
+      borderColor : "primary"
+    },
+  };
+
   return (
-    <></>
+    <>
+      <Typography sx={styles.studentName}>Ладыгин Даниил Львович</Typography>
+      <List sx={styles.list}>
+        <Typography sx={styles.courses}>Предметы</Typography>
+        <ListItem sx={styles.listItem}>
+          <Typography sx={styles.courseTitle}>Математика</Typography>
+          <Stack direction='row' spacing={2}>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+          </Stack>
+        </ListItem>
+        <ListItem sx={styles.listItem}>
+          <Typography sx={styles.courseTitle}>Информатика</Typography>
+          <Stack direction='row' spacing={2}>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+          </Stack>
+        </ListItem>
+        <ListItem sx={styles.listItem}>
+          <Typography sx={styles.courseTitle}>Биология</Typography>
+          <Stack direction='row' spacing={2}>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+          </Stack>
+        </ListItem>
+        <ListItem sx={styles.listItem}>
+          <Typography sx={styles.courseTitle}>Физика</Typography>
+          <Stack direction='row' spacing={2}>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+          </Stack>
+        </ListItem>
+        <ListItem sx={styles.listItem}>
+          <Typography sx={styles.courseTitle}>Химия</Typography>
+          <Stack direction='row' spacing={2}>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+            <Typography sx={styles.grade}>5</Typography>
+          </Stack>
+        </ListItem>
+      </List>
+    </>
   );
 };
 
