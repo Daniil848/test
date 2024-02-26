9ed5de47d5de95e1b4f96fdef493f6e21ec97bdf
Daniil848 <ladygindaniil848@gmail.com>
2024-02-26 11:03:53 +0300

    props types

--- src/UI/Input.tsx
+++ src/UI/Input.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
-const Input = (props: any) => {
+const Input = (props: {
+  label: string;
+  type: string;
+  placeholder: string;
+  value: string | number;
+  defaultValue: string | number;
+  onChange:
+    | React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+    | undefined;
+}) => {
   return (
     <>
       <div className={styles.container}>
