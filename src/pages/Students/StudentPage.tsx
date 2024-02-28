e8fb3ecae8d82b912daad38b01633a9264d7aab5
Daniil848 <ladygindaniil848@gmail.com>
2024-02-28 14:35:14 +0300

    student page center content

--- src/pages/Students/StudentPage.tsx
+++ src/pages/Students/StudentPage.tsx
@@ -10,6 +10,7 @@ import {
   TableCell,
   Box,
 } from '@mui/material';
+import styles from './StudentPage.module.scss';
 
 const StudentPage = () => {
   const { state, countVisits, styles } = useStudentPage();
@@ -17,54 +18,58 @@ const StudentPage = () => {
   if (!state.student) return null;
   return (
     <>
-      <Box sx={styles.box}>
-        <Typography sx={styles.studentName}>{state.student.name}</Typography>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Предмет</TableCell>
-                <TableCell sx={styles.tableCell}>Оценки</TableCell>
-                <TableCell sx={styles.tableCell}>Был(а) на знанятиях</TableCell>
-                <TableCell sx={styles.tableCell}>Пропустил(а)</TableCell>
-                <TableCell sx={styles.tableCell}>
-                  Пропустил(а) без причины
-                </TableCell>
-                <TableCell sx={styles.tableCell}>Средний балл</TableCell>
-                <TableCell sx={styles.tableCell}>Зачет</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {state.studentsGrades.map((studentGrades) => (
-                <TableRow key={studentGrades.id}>
-                  <TableCell>
-                    {state.courses
-                      .filter((el) => el.id == studentGrades.courseId)
-                      .map((e) => e.name)}
-                  </TableCell>
+      <Box sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}>
+        <Box sx={styles.box}>
+          <Typography sx={styles.studentName}>{state.student.name}</Typography>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Предмет</TableCell>
+                  <TableCell sx={styles.tableCell}>Оценки</TableCell>
                   <TableCell sx={styles.tableCell}>
-                    {studentGrades.grades.join(', ')}
+                    Был(а) на знанятиях
                   </TableCell>
+                  <TableCell sx={styles.tableCell}>Пропустил(а)</TableCell>
                   <TableCell sx={styles.tableCell}>
-                    {countVisits(studentGrades.visiting, 1)} раз
+                    Пропустил(а) без причины
                   </TableCell>
-                  <TableCell sx={styles.tableCell}>
-                    {countVisits(studentGrades.visiting, 2)} раз
-                  </TableCell>
-                  <TableCell sx={styles.tableCell}>
-                    {countVisits(studentGrades.visiting, 3)} раз
-                  </TableCell>
-                  <TableCell sx={styles.tableCell}>
-                    {studentGrades.averageGrade.toFixed(2)}
-                  </TableCell>
-                  <TableCell sx={styles.tableCell}>
-                    {studentGrades.attestation ? 'Зачет' : 'Незачет'}
-                  </TableCell>
+                  <TableCell sx={styles.tableCell}>Средний балл</TableCell>
+                  <TableCell sx={styles.tableCell}>Зачет</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {state.studentsGrades.map((studentGrades) => (
+                  <TableRow key={studentGrades.id}>
+                    <TableCell>
+                      {state.courses
+                        .filter((el) => el.id == studentGrades.courseId)
+                        .map((e) => e.name)}
+                    </TableCell>
+                    <TableCell sx={styles.tableCell}>
+                      {studentGrades.grades.join(', ')}
+                    </TableCell>
+                    <TableCell sx={styles.tableCell}>
+                      {countVisits(studentGrades.visiting, 1)} раз
+                    </TableCell>
+                    <TableCell sx={styles.tableCell}>
+                      {countVisits(studentGrades.visiting, 2)} раз
+                    </TableCell>
+                    <TableCell sx={styles.tableCell}>
+                      {countVisits(studentGrades.visiting, 3)} раз
+                    </TableCell>
+                    <TableCell sx={styles.tableCell}>
+                      {studentGrades.averageGrade.toFixed(2)}
+                    </TableCell>
+                    <TableCell sx={styles.tableCell}>
+                      {studentGrades.attestation ? 'Зачет' : 'Незачет'}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
     </>
   );
